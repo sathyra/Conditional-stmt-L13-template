85028e0a0504b0b2465333fd1d53edc1b1331710
sathyra <105861021+sathyra@users.noreply.github.com>
2022-05-23 17:23:57 +0530

    Update o.py

--- o.py
+++ o.py
@@ -2,7 +2,7 @@
 #if statement
 marks_1 = 40
 marks_2 = 45
-if(marks_1 + marks_2 < 100):
+#fill the if statement
   print("Total marks is less than 100. Condition is true.")
 
 #else statement
@@ -10,7 +10,7 @@ if(marks_1 + marks_2 < 100):
 marks_2 = 45
 if(marks_1 + marks_2 < 100):
   print("Total marks is less than 100. Condition is true.")
-else: 
+#fill the else block
   print("Total marks is not less than 100. Else Condition is true.")
 
 
@@ -20,7 +20,4 @@ practical_points = 20
 
 if(theory_points > 50):
     print("input points for 'Theory' is correct.") 
-elif(practical_points > 50):
-    print("input points for 'Practical' is correct.")  
-else:
-    print("Points validated. Your total is: ",practical_points + theory_points)
+#fill the elif statment
